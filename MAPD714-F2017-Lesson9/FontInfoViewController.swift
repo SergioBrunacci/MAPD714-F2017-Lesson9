e9eb0a52a693dd98d4b6d93be5553556ed94167a
SergioBrunacci <sergio.a.brunacci@gmail.com>
2017-11-29 23:52:54 -0500

    Add Info, List and Sizes view controllers with codes

---
+++ MAPD714-F2017-Lesson9/FontInfoViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FontInfoViewController.swift
+//  MAPD714-F2017-Lesson9
+//
+//  Created by Sergio de Almeida Brunacci on 2017-11-29.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class FontInfoViewController: UIViewController {
+
+    var font: UIFont!
+    var favourite: Bool = false
+    @IBOutlet weak var fontSampleLabel: UILabel!
+    @IBOutlet weak var fontSizeSlider: UISlider!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+    @IBOutlet weak var favouriteSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        fontSampleLabel.font = font
+        fontSampleLabel.text = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVv" + "WwXxYyZz 0123456789"
+        fontSizeSlider.value = Float(font.pointSize)
+        fontSizeLabel.text = "\(Int(font.pointSize))"
+        favouriteSwitch.isOn = favourite
+        
+    }
+    
+    @IBAction func slideFontSize(slider: UISlider){
+        let newSize = roundf(slider.value)
+        fontSampleLabel.font = font.withSize(CGFloat(newSize))
+        fontSizeLabel.text = "\(Int(newSize))"
+    }
+    
+    @IBAction func toggleFavourite(sender: UISwitch){
+        let favouritesList = FavouritesList.sharedFavouritesList
+        if sender.isOn{
+            favouritesList.addFavourites(fontName: font.fontName)
+        }else{
+            favouritesList.removeFavourite(fontName: font.fontName)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
